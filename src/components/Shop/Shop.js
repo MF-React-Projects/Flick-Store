1a0515d8b251c97f9a18e747464fc8b461df8775
mehedifoysal <mehedifoysal8@gmail.com>
2022-03-26 14:55:38 +0600

    product data load

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react';
+import './Shop.css';
+import Product from "../Product/Product";
+import {Container, Row} from "react-bootstrap";
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+
+    return (
+        <div>
+            <div className="ic-products">
+                <div className="ic-grid-products">
+                    <Container>
+                        <Row>
+                            {
+                                products.map(product => <Product key={product.id} product={product}/>)
+                            }
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
